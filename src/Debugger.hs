be37a99d73724c1fe913e0c9ff66e6e5fe8a62a4
doyougnu <jeffrey.young@iohk.io>
2024-08-18 11:25:21 -0400

    wrk: add debugger class and monad

--- src/Debugger.hs
+++ src/Debugger.hs
@@ -1,3 +1,8 @@
+{-# LANGUAGE  DeriveAnyClass             #-}
+{-# LANGUAGE  DerivingStrategies         #-}
+{-# LANGUAGE  FunctionalDependencies     #-}
+{-# LANGUAGE  ScopedTypeVariables        #-}
+
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Debugger
@@ -16,23 +21,83 @@
 
 
 module Debugger
-  ( enterDebugger
+  ( -- enterDebugger
   ) where
+
+import Evaluator
+
+import Data.Bifunctor
+import Control.Monad.Reader (ReaderT, MonadReader)
+import Control.Monad.IO.Class
+import Control.Monad.Error.Class
+import qualified Control.Monad.Reader as R
+
 
 
 -- -----------------------------------------------------------------------------
 -- Types
 
-newtype Debug a = Debug
-  { runDebug :: ReaderT DebugContext (ExceptT ExpansionErr IO) a
-  }
-  deriving ( Functor, Applicative, Monad
-           , MonadError ExpansionErr
-           , MonadIO, MonadReader DebugContext
-           )
+-- conceptually this is a ReaderT (DebugContext e) (ExceptT e) IO a but I've
+-- just fused the transformers and to have more control over the monad instance
+newtype Debug r e a = Debug { runDebugT :: r -> IO (Either e a)
+                              }
+
+runDebug :: Debug r e a -> r -> IO (Either e a)
+runDebug = runDebugT
+
+debugRunT :: r -> Debug r e a -> IO (Either e a)
+debugRunT = flip runDebugT
+
+mapDebugT :: (a -> b) -> Debug r e a -> Debug r e b
+mapDebugT f = Debug . fmap (fmap (second f)) . runDebugT
+
+instance Functor (Debug r e) where
+  fmap = mapDebugT
+
+instance Applicative (Debug r e) where
+  pure a  = Debug $ const (return (Right a))
+  Debug f <*> Debug v = Debug $ \rr -> do
+    mf <- f rr
+    case mf of
+      (Left fer) -> return (Left fer)
+      (Right k)  -> do
+        mv <- v rr
+        case mv of
+          (Left ver) -> return (Left ver)
+          Right x    -> return (Right (k x))
+
+instance Monad (Debug r e) where
+  Debug m >>= f  = Debug $ \r -> do
+    ma <- m r
+    case ma of
+      Left err  -> return (Left err)
+      Right val -> fmap (debugRunT r) f val
+
+instance MonadIO (Debug r e) where
+  liftIO = Debug . const . fmap Right
+
+class (Monad io, MonadIO io) => MonadDebugger e io | io -> e where
+  -- conceptually this is throw
+  enter :: e -> m a
+  -- conceptually this is catch with a handler
+  catch :: m a -> (e -> m b) -> m b
+
+
+data DebugContext e = DebugContext { _currentError :: Maybe e
+                                   , _stackTrace   :: [EState]
+                                   }
+                      deriving (Semigroup, Monoid)
+
+initialContext :: DebugContext e
+initialContext = mempty
+
+
+-- checkError :: Debug e (Maybe e)
+-- checkError = R.asks _currentError
+
 
 -- -----------------------------------------------------------------------------
 -- Top level API
 
-enterDebugger :: ExpansionErr -> EState -> Debug Value
-enterDebugger exp_err st = undefined
+-- enterDebugger :: ExpansionErr -> EState -> Debug Value
+-- enterDebugger exp_err st =
